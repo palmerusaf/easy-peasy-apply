82796462eb683a36aa5525b402c9fad27cbff5d1
Branden Palmer <brandenpalmer08@gmail.com>
2023-11-10 16:52:08 -0600

    remove easy apply timeout to prevent stopping on slow connection

--- easy-apply.js
+++ easy-apply.js
@@ -211,7 +211,7 @@ async function applyForJob() {
   }
   else {
     // Close Easy Apply Modal
-    await easyApplyModal.locator('[data-test-modal-close-btn]').click({ timeout: 3000 })
+    await easyApplyModal.locator('[data-test-modal-close-btn]').click({ timeout: 0 })
     await randomWait(2000, 3000)
     await page
       .locator('[data-test-modal-id="data-test-easy-apply-discard-confirmation"]')
